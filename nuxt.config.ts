d5d7c5710f12f63ee89d6f4e9fed75c7f00f6f46
KiAi <94138724+broccoli88@users.noreply.github.com>
2024-10-07 20:09:45 +0200

    up

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,34 +1,42 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-	devtools: { enabled: false },
-	css: ["~/assets/scss/main.scss"],
-	pages: true,
-	vite: {
-		css: {
-			preprocessorOptions: {
-				scss: {
-					additionalData:
-						'@import "@/assets/scss/_variables.scss"; @import "@/assets/scss/_mixins.scss";',
-				},
-			},
-		},
+ devtools: { enabled: false },
+ css: ["~/assets/scss/main.scss"],
+ pages: true,
+
+ vite: {
+					css: {
+									preprocessorOptions: {
+													scss: {
+																	additionalData:
+																					'@import "@/assets/scss/_variables.scss"; @import "@/assets/scss/_mixins.scss";',
+													},
+									},
+					},
 	},
-	modules: ["@pinia/nuxt", "@nuxtjs/supabase", "nuxt-icon", "@nuxt/image"],
-	components: [
-		{
-			path: "~/components",
-			pathPrefix: false,
-		},
+
+ modules: ["@pinia/nuxt", "@nuxtjs/supabase", "nuxt-icon", "@nuxt/image"],
+
+ components: [
+					{
+									path: "~/components",
+									pathPrefix: false,
+					},
 	],
-	runtimeConfig: {
-		supabasr_url: process.env.SUPABASE_URL,
-		supabasle_api_key: process.env.SUPABASE_API_KEY,
+
+ runtimeConfig: {
+					supabasr_url: process.env.SUPABASE_URL,
+					supabasle_api_key: process.env.SUPABASE_API_KEY,
 	},
-	supabase: {
-		redirect: false,
+
+ supabase: {
+					redirect: false,
 	},
-	image: {
-		format: ["webp", "avif", "png", "jpeg", "jpg"],
-		densities: [1, 2],
+
+ image: {
+					format: ["webp", "avif", "png", "jpeg", "jpg"],
+					densities: [1, 2],
 	},
+
+ compatibilityDate: "2024-10-07",
 });
